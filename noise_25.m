cc0f2a9ab15d3bc9920c38dd601d012cbea48e58
Cuiqing Zhang <73119756+CuiqingZhang@users.noreply.github.com>
2020-10-20 00:38:59 +0800

    Update noise_25.m

--- noise_25.m
+++ noise_25.m
@@ -3,7 +3,7 @@ x0=textscan(fid ,'%f%f%f%f%f%f%f%f%f');
 fclose(fid);
 x1=cell2mat(x0);
 x2=(x1(:,[1,2,3,4,5,6,7]));
-x_noise=x2+10*randn(size(x2))+0;
+x_noise=0.8*(x2+*randn(size(x2))+0)+0.2*(x2+10*randn(size(x2))+0);
 z=round(rand(1,1044)*4176);
 for i=1:1:1044
     x2(z(i),:)=x_noise(z(i),:);
